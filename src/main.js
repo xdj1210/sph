791abe99d88c25903af26c9986a2d4ce03fa6f8d
xdj <117930784@qq.com>
2023-11-11 11:09:14 +0800

    home静态页面搭建

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import Header from './components/header'
 import Footer from './components/footer'
+import TypeNav from './components/typeNav'
 import '@/assets/reset.css'
 import router from '@/router'
 Vue.config.productionTip = false
 Vue.component(Header.name,Header)
 Vue.component(Footer.name,Footer)
+Vue.component(TypeNav.name,TypeNav)
 new Vue({
   render: h => h(App),
   router
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,10 +1,33 @@
 <template>
-  <div>首页</div>
+  <div>
+    <TypeNav></TypeNav>
+    <List></List>
+    <Recommend></Recommend>
+    <Rank></Rank>
+    <Like></Like>
+    <Floor></Floor>
+    <Floor></Floor>
+    <Brand></Brand>
+  </div>
 </template>
 
 <script>
+import List from './list'
+import Recommend from './recommend'
+import Rank from './rank'
+import Like from './like'
+import Floor from './floor'
+import Brand from './brand'
   export default {
-    name:'Home'
+    name:'Home',
+    components:{
+      List,
+      Recommend,
+      Rank,
+      Like,
+      Floor,
+      Brand
+    }
   }
 </script>
 
